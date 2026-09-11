a968e83ae18de930dec7b2aeb9e328dca45f8a09
Dhvani Vora <dhvani.vora@tatvasoft.com>
2026-09-11 15:15:42 +0530

    removed the unwanted files

--- src/main/java/com/tatvasoft/interview_portal/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.tatvasoft.interview_portal.service;
-
-import com.tatvasoft.interview_portal.dto.RoleResponse;
-
-import java.util.List;
-
-public interface RoleService {
-    List<RoleResponse> getAllRoles();
-}
